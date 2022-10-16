f9f28524cb318a1b3c658516df11a3a1212577e1
Ralf D. Müller <ralf.d.mueller@gmail.com>
2022-10-16 16:03:38 +0000

    added switchToreq42.sh

---
+++ switchToreq42.sh
@@ -0,0 +1,4 @@
+sed -i 's/arc42-template/req42-framework/g' build.gradle
+sed -i 's/arc42-template/req42-framework/g' buildconfig.groovy
+sed -i 's/arc42-template/req42-framework/g' settings.gradle
+sed -i 's/arc42-template/req42-framework/g' subBuild.gradle
