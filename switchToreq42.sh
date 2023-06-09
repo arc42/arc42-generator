9667523eb0322ae99b8241164857cb65916706c9
Ralf D. Müller <ralf.d.mueller@gmail.com>
2023-06-09 17:35:00 +0200

    added needed steps to build req42

--- switchToreq42.sh
+++ switchToreq42.sh
@@ -2,3 +2,13 @@ sed -i 's/arc42-template/req42-framework/g' build.gradle
 sed -i 's/arc42-template/req42-framework/g' buildconfig.groovy
 sed -i 's/arc42-template/req42-framework/g' settings.gradle
 sed -i 's/arc42-template/req42-framework/g' subBuild.gradle
+rm -r build
+cd req42-framework
+git checkout main
+git pull
+cd ..
+./gradlew createTemplatesFromGoldenMaster
+./gradlew arc42
+./gradlew createDistribution
+echo "please check the results in req42-framework/dist"
+echo "and if ok, add, commit and push it"
