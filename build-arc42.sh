85d4886fc0f5be22e862bcae82c3f76aa91b3008
Ralf D. Müller <ralf.d.mueller@gmail.com>
2025-07-25 20:46:23 +0000

    ref #52: install pandoc and copy common folder

--- build-arc42.sh
+++ build-arc42.sh
@@ -1,9 +1,15 @@
+echo "Building arc42 template"
+echo "install pandoc"
+wget https://github.com/jgm/pandoc/releases/download/3.7.0.2/pandoc-3.7.0.2-1-amd64.deb
+sudo dpkg -i pandoc-3.7.0.2-1-amd64.deb
+echo "init and updatesubmodules
 git submodule init
 git submodule update
 cd arc42-template
 git checkout master
 git pull
 cd ..
+echo "build arc42 template"
 ./gradlew createTemplatesFromGoldenMaster
 ./gradlew arc42
 ./gradlew createDistribution
